bd24741e0ca283fb14e908e690b7af29053a85ba
John Hughes <woodenhorse3@gmail.com>
2018-03-22 20:14:51 -0700

    added runcipher.py
    added the runcipher.py file, this will run the class cipher and start the whole program.

---
+++ runcipher.py
@@ -0,0 +1,10 @@
+# runcipher.py file by John Hughes 3/9/18
+
+from ciphers import Cipher
+from os import sys
+
+if __name__ == "__main__":
+        cipher = Cipher()
+        sys.exit(0)
+        
+
